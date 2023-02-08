9421aae27ceecdce211fe38e3a2cfb21fb35b7ab
songyejin <yjsong0088@gmail.com>
2023-02-08 18:14:39 +0900

    first commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        if (a >= 90)
+            System.out.println("A");
+        else if (a >= 80)
+            System.out.println("B");
+        else if (a >= 70)
+            System.out.println("C");
+        else if (a >= 60)
+            System.out.println("D");
+        else
+            System.out.println("F");
+
+    }
+}
