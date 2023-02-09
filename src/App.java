3471e7994901eee034249156e144c614bf18ac41
songyejin <yjsong0088@gmail.com>
2023-02-09 21:11:01 +0900

    second

--- src/App.java
+++ src/App.java
@@ -1,20 +1,16 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(br.readLine());
-        if (a >= 90)
-            System.out.println("A");
-        else if (a >= 80)
-            System.out.println("B");
-        else if (a >= 70)
-            System.out.println("C");
-        else if (a >= 60)
-            System.out.println("D");
-        else
-            System.out.println("F");
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        double a = Double.parseDouble(st.nextToken());
+        double b = Double.parseDouble(st.nextToken());
+
+        System.out.println(a / b);
     }
 }
